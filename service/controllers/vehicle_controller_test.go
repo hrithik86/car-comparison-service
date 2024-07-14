248e69499a03afb15b6fb697b58f16da305e582c
hrithik86 <hrithikmalik86@gmail.com>
2024-07-14 23:01:25 +0530

    add test cases for get vehicles by name

--- service/controllers/vehicle_controller_test.go
+++ service/controllers/vehicle_controller_test.go
@@ -66,6 +66,45 @@ func (v *VehicleServiceTestSuite) TestVehicle_GetVehicleInfoById_Failure() {
 				v.mockDb.EXPECT().GetVehicleWithFeaturesById(gomock.Any(), id).Times(1).Return(nil, errors.RECORD_NOT_FOUND)
 				vehicleInfo, err := v.vehicleClient.GetVehicleInfoById(context.Background(), id)
 				So(err, ShouldNotBeNil)
+				So(err, ShouldResemble, errors.RECORD_NOT_FOUND)
+				So(vehicleInfo, ShouldResemble, expectedResponse)
+			})
+		})
+	})
+}
+
+func (v *VehicleServiceTestSuite) TestVehicle_GetVehiclesByModelName_Success() {
+	Convey("Given valid model", v.T(), func() {
+		Convey("When get like model name is called", func() {
+			Convey("Then it should return the data ", func() {
+
+				vehicleData := getVehicleMockData()
+				expectedResponse := []*model.VehicleWithAttachmentInformation{{
+					Vehicle:      *vehicleData,
+					AttachmentId: nil,
+					MediaType:    nil,
+					Path:         nil,
+				}}
+				v.mockDb.EXPECT().GetVehiclesByModel(gomock.Any(), utils.GetValFromPtr(vehicleData.Model)).Times(1).Return(expectedResponse, nil)
+				vehicleInfo, err := v.vehicleClient.GetVehiclesByModelName(context.Background(), utils.GetValFromPtr(vehicleData.Model))
+				So(err, ShouldBeNil)
+				So(vehicleInfo, ShouldResemble, expectedResponse)
+			})
+		})
+	})
+}
+
+func (v *VehicleServiceTestSuite) TestVehicle_GetVehiclesByModelName_Failure() {
+	Convey("Given invalid model", v.T(), func() {
+		Convey("When get like model name is called", func() {
+			Convey("Then it should throw error ", func() {
+
+				vehicleData := getVehicleMockData()
+				var expectedResponse []*model.VehicleWithAttachmentInformation
+				v.mockDb.EXPECT().GetVehiclesByModel(gomock.Any(), utils.GetValFromPtr(vehicleData.Model)).Times(1).Return(nil, errors.RECORD_NOT_FOUND)
+				vehicleInfo, err := v.vehicleClient.GetVehiclesByModelName(context.Background(), utils.GetValFromPtr(vehicleData.Model))
+				So(err, ShouldNotBeNil)
+				So(err, ShouldResemble, errors.RECORD_NOT_FOUND)
 				So(vehicleInfo, ShouldResemble, expectedResponse)
 			})
 		})
